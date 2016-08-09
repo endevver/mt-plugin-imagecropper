4a0ed5dc88c949feccde31eb34a287ee8c56a80a
Dan Wolfgang <dan@uinnovations.com>
2016-08-09 15:42:03 -0400

    When publishing a template, supply a dynamic URL if the desired protytpe hasn’t built an asset yet.

--- plugins/ImageCropper/lib/ImageCropper/Tags.pm
+++ plugins/ImageCropper/lib/ImageCropper/Tags.pm
@@ -4,11 +4,14 @@ use strict;
 use warnings;
 
 use ImageCropper::Util qw( find_cropped_asset );
+use MT::Util qw( caturl );
 
 # CroppedAsset tag
 sub hdlr_cropped_asset {
     my ( $ctx, $args, $cond ) = @_;
-    my $l       = $args->{label};
+    my $app     = MT->instance;
+    my $label   = $args->{label};
+    my $no_autocrop = $args->{no_autocrop} || 1;
 
     my $a       = $ctx->stash('asset')
         or return $ctx->_no_asset_error();
@@ -22,9 +25,25 @@ sub hdlr_cropped_asset {
                 : 0;
 
     my $out;
-    my $cropped_asset = find_cropped_asset($blog_id, $a, $l);
+    my $cropped_asset = find_cropped_asset($blog_id, $a, $label, $no_autocrop);
     if ($cropped_asset) {
         local $ctx->{__stash}{'asset'} = $cropped_asset;
+
+        # Is there an AutoCrop job in the queue? If so, we need to supply a new
+        # URL to use the `find_asset_url.cgi` interface. (And if there is no
+        # AutoCrop job that means the asset already exists, or it's not
+        # automatically created and this URL rewrite is skipped.)
+        if ( $app->model('ts_job')->exist({ uniqkey => $a->id }) ) {
+            $ctx->{__stash}{asset}->url(
+                caturl(
+                    $app->config->CGIPath,
+                    $app->component('ImageCropper')->envelope,
+                    $app->config->FindAssetURLScript
+                        . '?asset_id=' . $a->id,
+                )
+            );
+        }
+
         defined( $out = $ctx->slurp( $args, $cond ) ) or return;
         return $out;
     }
